06c22dea88b3bea9de09db45913d3f06e792a8a6
StefanoBisi <stefano.bisi@aol.com>
2026-08-10 15:06:39 +0200

    squirrelvania: basic collisions implemented

--- src/rect.lua
+++ src/rect.lua
@@ -1,23 +1,28 @@
 function coordinates(x_, y_) return {x=x_, y=y_} end
 function size(w_, h_) return {w=w_, h=h_} end
+Body = {
+    make = function(x_, y_, w_, h_, s_)
+        local _body = {
+            pos = coordinates(x_, y_),
+            size = size(w_, h_),
+            speed = coordinates(0, 0),
+            sprite = s_
+        }
 
-function in_rect(pos, rect)
-    return pos.x > rect.pos.x and pos.x < rect.pos.x + rect.size.w
-        and pos.y > rect.pos.y and pos.y < rect.pos.y + rect.size.h
+        return _body
+    end
+}
+function right(body) return body.pos.x + body.size.w - 1 end
+function bottom(body) return body.pos.y + body.size.h - 1 end
+
+function horizontal_overlap(body_a, body_b)
+    return (body_a.pos.x >= body_b.pos.x and body_a.pos.x <= right(body_b))
+    or (body_b.pos.x >= body_a.pos.x and body_b.pos.x <= right(body_a))
+end
+function vertical_overlap(body_a, body_b)
+    return (body_a.pos.y >= body_b.pos.y and body_a.pos.y <= bottom(body_b))
+    or (body_b.pos.y >= body_a.pos.y and body_b.pos.y <= bottom(body_a))
 end
 function intersect(body_a, body_b)
-    return in_rect(body_a.pos, body_b) or in_rect(body_b.pos, body_a)
-end
-
-function left_adjacent(body_a, body_b)
-    return body_a.pos.x + body_a.size.w == body_b.pos.x
-end
-function right_adjacent(body_a, body_b)
-    return body_a.pos.x == body_b.pos.x + body_b.size.w
-end
-function down_adjacent(body_a, body_b)
-    return body_a.pos.y + body_a.size.h == body_b.pos.y
-end
-function up_adjacent(body_a, body_b)
-    return body_a.pos.y == body_b.pos.y + body_b.size.h
+    return horizontal_overlap(body_a, body_b) and vertical_overlap(body_a, body_b)
 end
--- src/squirrelvania.lua
+++ src/squirrelvania.lua
@@ -1,63 +1,88 @@
 function _init()
-    player = {
-        pos = coordinates(64, 64),
-        size = size(7,7),
-        speed = coordinates(0, 0),
-        on_ground = false
-    }
-    camera = coordinates(0, 0)
+    player = Body.make(64, 64, 8, 4, 0)
+    player.on_ground = false
+    --player.speed.y = 1
+    camera = Body.make(0, 0, 128, 128)
     world = {
-        { sprite = 1, pos = coordinates(32, 102), size = size(8,8) },
-        { sprite = 1, pos = coordinates(96, 102), size = size(8,8) }
+        Body.make(-1, 0, 1, 130), -- left limit
+        Body.make(159, 1, 1, 130), -- right limit
+        Body.make(0, 110, 160, 24), -- bottom limit
+        Body.make(32, 102, 8, 8, 1),
+        Body.make(96, 102, 8, 8, 1)
     }
 end
 
-function check_speed_boundaries(pos, speed_delta)
-    if speed_delta > 0 then
-        if pos.x >= 150 then return 0 end
+function check_speed_boundaries(body)
+    function fix_speed(overlap_fn, dist_fn, axis)
+        local dist = 128
+        for elem in all(world) do
+            local d = dist_fn(body, elem)
+            if overlap_fn(body, elem) and d >= 0 then
+                dist = min(dist, d)
+            end
+        end
+        m = (body.speed[axis] < 0 and -1 or 1)
+        if dist < (m * body.speed[axis]) then
+            body.speed[axis] = m * min(min(m * body.speed[axis], dist - 1), 0)
+        end
     end
-    if speed_delta < 0 then
-        if pos.x <= 0 then return 0 end
+
+    if body.speed.x > 0 then
+        fix_speed(vertical_overlap, function (a, b) return b.pos.x - right(a) end, "x")
+    elseif body.speed.x < 0 then
+        fix_speed(vertical_overlap, function (a, b) return a.pos.x - right(b) end, "x")
     end
-    return speed_delta
+    
+    if body.speed.y > 0 then
+        fix_speed(horizontal_overlap, function (a, b) return b.pos.y - bottom(a) end, "y")
+    elseif body.speed.y < 0 then
+        fix_speed(horizontal_overlap, function (a, b) return a.pos.y - bottom(b) end, "y")
+    end
+end
+
+function on_ground(body)
+    for elem in all(world) do
+        if horizontal_overlap(body,elem) and elem.pos.y - bottom(body) == 1 then
+            body.on_ground = true
+            return
+        end
+    end
+    body.on_ground = false
 end
 
 function _update()
-    player.on_ground = player.pos.y >= 104
+    on_ground(player)
     if player.on_ground then
+        player.speed.y = 0
         if btn(❎) then
-            player.speed.y = -4
+            player.speed.y = -5
         else
-            player.speed = { x = 0, y = 0 }
-            speed_delta = 0
-            if btn(➡️) then speed_delta = 2.0 end
-            if btn(⬅️) then speed_delta = -2.0 end
-            player.speed.x = check_speed_boundaries(player.pos, speed_delta)
+            if btn(➡️) then player.speed.x = 2.0
+            elseif btn(⬅️) then player.speed.x = -2.0
+            else player.speed.x = 0.0 end
         end
     else
-        player.speed.y += 0.5
+        player.speed.y = player.speed.y + 1
     end
+    check_speed_boundaries(player)
 
     player.pos.x += player.speed.x
-    camera.x += player.speed.x
+    camera.pos.x += player.speed.x
 
     player.pos.y += player.speed.y
-    if player.pos.y < 0 then player.pos.y = 0 end
-    if player.pos.y > 104 then player.pos.y = 104 end
 end
 
-function draw(sprite, pos)
-    local screen_pos = coordinates(pos.x - camera.x, pos.y - camera.y)
-    if screen_pos.x > 0 and screen_pos.x < 128 and screen_pos.y > 0 and screen_pos.y < 128 then
-        spr(sprite, screen_pos.x, screen_pos.y)
-    end
+function draw(body)
+    if not intersect(body, camera) then return end 
+    spr(body.sprite, body.pos.x - camera.pos.x, body.pos.y - camera.pos.y)
 end
 
 function _draw()
     rectfill(0, 0, 128, 128, 1)
-    rectfill(0, 104, 128, 128, 3)
-    draw(0, player.pos) 
-    foreach(world, function(block) draw(block.sprite, block.pos) end)
-    print("pos "..player.pos.x, 1,1)
-    print(" speed "..player.speed.x, 1, 11)
+    rectfill(0, 110, 128, 128, 3)
+    foreach(world, function(block) if block.sprite then draw(block) end end)
+    draw(player)
+    print("pos: "..player.pos.x.."x"..player.pos.y, 1, 1 ,7)
+    print("speed: "..player.speed.x.."x"..player.speed.y, 1, 11, 7)
+    print("camera:"..camera.pos.x.."x"..camera.pos.y,1, 21, 7)
 end
